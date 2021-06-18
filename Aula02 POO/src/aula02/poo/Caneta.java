8a903de6975f5d9a01ca65864fc22d869578f95c
Alisson <74628792+alissonFabricio04@users.noreply.github.com>
2021-06-18 10:25:48 -0300

    Add files via upload

---
+++ Aula02 POO/src/aula02/poo/Caneta.java
@@ -0,0 +1,33 @@
+package aula02.poo;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta + "mm");
+        System.out.println("Carga: " + this.carga + "%");
+        System.out.println("Esta tampada? " + this.tampada);
+    }
+    
+    void rabisar() {
+        if(this.tampada == true) {
+            System.out.println("N posso rabiscar, esta tampada");
+        } else {
+            System.out.println("Estou rabiscando");
+        }
+    }
+    
+    void tampar() {
+        this.tampada = true;
+    }
+    
+    void destampar() {
+        this.tampada = false;
+    }
+}
